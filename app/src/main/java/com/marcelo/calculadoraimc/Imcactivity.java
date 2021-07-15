4edb5213964f8e636c7576f470d6f9d98a6148f4
Marcelo Santos <rocha.mer21@gmail.com>
2021-07-15 17:44:58 -0300

    criado a splash screen.

---
+++ app/src/main/java/com/marcelo/calculadoraimc/Imcactivity.java
@@ -0,0 +1,14 @@
+package com.marcelo.calculadoraimc;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Imcactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imcactivity);
+    }
+}
