b836eae7c73dd17569649e65c05c7dd2abe918f6
Marcelo Santos <rocha.mer21@gmail.com>
2021-07-15 17:25:16 -0300

    Teminado o layout principal, add imagens e colocado em seus respectivo lugares.

---
+++ app/src/main/java/com/marcelo/calculadoraimc/MainActivity.java
@@ -0,0 +1,14 @@
+package com.marcelo.calculadoraimc;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
