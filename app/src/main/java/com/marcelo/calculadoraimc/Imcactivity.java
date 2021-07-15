52107dc83982147fb732aaee63e43bb4a3050c80
Marcelo Santos <rocha.mer21@gmail.com>
2021-07-15 18:49:11 -0300

    imc activity quase finalizada, feito a função que retorna a activity de calculo do imc

--- app/src/main/java/com/marcelo/calculadoraimc/Imcactivity.java
+++ app/src/main/java/com/marcelo/calculadoraimc/Imcactivity.java
@@ -1,14 +1,32 @@
 package com.marcelo.calculadoraimc;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 public class Imcactivity extends AppCompatActivity {
+
+    AppCompatButton mrecalculatebmi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imcactivity);
+
+        getSupportActionBar().hide();
+
+        mrecalculatebmi = findViewById(R.id.recalculatebmi);
+
+        mrecalculatebmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Imcactivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
